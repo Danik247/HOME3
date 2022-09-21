351507475746cd229f5e04491ca4644123babd16
home1 <kazorind@gmail.com>
2022-09-21 19:34:03 +0300

    comit

---
+++ main.py
@@ -0,0 +1,30 @@
+name = "Danik"
+
+#2.3
+print("2.3")
+print ("Hello,", name+\
+       ", it's ur homework")
+
+#2.4
+name = "    danik  / "
+print("2.4")
+print(name.lower(),"все буквы в нижнем регистре")
+print(name.upper(),"все буквы в верхнем  регистре")
+print(name.title(),"первая буква заглавная ")
+
+#2.5
+print("2.5")
+print("CJ once said, 'Ah, shit, here we go again'")
+
+#2.6
+print("2.6")
+name = "CJ"
+print(name, "once said, 'Ah, shit, here we go again'")
+
+#2.7
+print("2.7")
+word = "\tabrikos\nfonarik"
+print(word)
+print(str.strip(word))
+print(str.lstrip(word))
+print(str.rstrip(word))
