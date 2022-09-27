292da62edb10b57a05851ddca8f99d3d88d8e8a9
home1 <kazorind@gmail.com>
2022-09-27 18:08:27 +0300

    comit

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-name = "Danik"
-
-#2.3
-print("2.3")
-print ("Hello,", name+\
-       ", it's ur homework")
-
-#2.4
-name = "    danik  / "
-print("2.4")
-print(name.lower(),"все буквы в нижнем регистре")
-print(name.upper(),"все буквы в верхнем  регистре")
-print(name.title(),"первая буква заглавная ")
-
-#2.5
-print("2.5")
-print("CJ once said, 'Ah, shit, here we go again'")
-
-#2.6
-print("2.6")
-name = "CJ"
-print(name, "once said, 'Ah, shit, here we go again'")
-
-#2.7
-print("2.7")
-word = "\tabrikos\nfonarik"
-print(word)
-print(str.strip(word))
-print(str.lstrip(word))
-print(str.rstrip(word))
+tanks = ['AMX 65t', 'AMX M4 54']
+message1 =('Я хотел бы покатать на '+tanks[1])
+message2 =('Я не хотел бы катать на '+tanks[0])
+print(message1)
+print(message2)
